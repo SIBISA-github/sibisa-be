509cf603ffb3d993bca275ebbc75718e0ffc7ef4
Dwi Bagus <dwibagus154@gmail.com>
2022-05-16 09:08:17 +0700

    fix: swagger and add cors

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express')
+const cors = require('cors')
 
 // Load Environment Variables
 require('dotenv').config()
 
 const app = express()
+app.use(cors())
 const { userRouters } = require('./src/routers')
+
+// Swagger
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./src/swagger/swagger.json')
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.use('/user', userRouters)
+app.use('/api/v1/user', userRouters)
 
 app.listen(process.env.PORT || 5000, () => console.log('connection success'))
--- src/routers/userRouters.js
+++ src/routers/userRouters.js
@@ -2,7 +2,7 @@ const express = require('express')
 const userController = require('../controllers/userController')
 
 const routers = express.Router()
-
+console.log(1)
 routers.get('/', userController.getAllUsers)
 
 module.exports = routers
