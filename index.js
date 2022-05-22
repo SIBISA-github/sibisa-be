aecf2fbd631af33fe4384ca04dd2210ff171b425
Faster112 <muhammadtauchid@gmail.com>
2022-05-22 22:49:24 +0700

    add config for question api

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express')
+const cors = require('cors')
+
+require('dotenv').config()
+
 const app = express()
+app.use(cors())
+const { questionRouters } = require('./src/routers')
+
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./src/swagger/swagger.json')
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(process.env.PORT || 5000, () => console.log('connection success'))
+app.use('/api/v1/question', questionRouters)
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server running on port ${process.env.PORT || 5000}`)
+})
