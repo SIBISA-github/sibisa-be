e0eb5a3467a27d8af5c8d11d62b87d2988f67541
Dwi Bagus <dwibagus154@gmail.com>
2022-05-24 08:25:28 +0700

    feat: add token for api

--- src/controllers/questionController.js
+++ src/controllers/questionController.js
@@ -1,54 +1,87 @@
 const questionServices = require('../services/questionServices')
+const Tokenization = require('../utils/jwtToken/tokenization')
 const Response = require('../utils/response/response')
 
 class questionController {
-    static async getAllQuestions(req, res) {
-        try {
-            const questions = await questionServices.getAllQuestions()
-            
-            if (!questions) throw new Error('Error while getting questions')
+  static async getAllQuestions (req, res) {
+    const token = req.headers.authorization
 
-            const response = Response.successResponse(200, 'Questions retrieved successfully', questions)
-            return res.status(200).send(response)
-        } catch (err) {
-            const message = err.message.replace(/['"]+/g, '')
-            const response = Response.badResponse(message, 400)
+    try {
+      // Check if token exist
+      if (!token) throw new Error('Token is required')
 
-            return res.status(400).send(response)
-        }
+      // validate token
+      const authorization = Tokenization.verifyToken(token)
+
+      if (!authorization) throw new Error('Invalid token')
+
+      const questions = await questionServices.getAllQuestions()
+
+      if (!questions) throw new Error('Error while getting questions')
+
+      const response = Response.successResponse(200, 'Questions retrieved successfully', questions)
+
+      return res.status(200).send(response)
+    } catch (err) {
+      const message = err.message.replace(/['"]+/g, '')
+
+      const response = Response.badResponse(message, 400)
+
+      return res.status(400).send(response)
     }
+  }
 
-    static async getQuestionById(req, res) {
-        try {
-            const question = await questionServices.getQuestionById(req.params.id)
-            
-            if (!question) throw new Error('Error while getting questions')
+  static async getQuestionById (req, res) {
+    const token = req.headers.authorization
 
-            const response = Response.successResponse(200, 'Questions retrieved successfully', question)
-            return res.status(200).send(response)
-        } catch (err) {
-            const message = err.message.replace(/['"]+/g, '')
-            const response = Response.badResponse(message, 400)
+    try {
+      // Check if token exist
+      if (!token) throw new Error('Token is required')
 
-            return res.status(400).send(response)
-        }
+      // validate token
+      const authorization = Tokenization.verifyToken(token)
+
+      if (!authorization) throw new Error('Invalid token')
+
+      const question = await questionServices.getQuestionById(req.params.id)
+
+      if (!question) throw new Error('Error while getting questions')
+
+      const response = Response.successResponse(200, 'Questions retrieved successfully', question)
+
+      return res.status(200).send(response)
+    } catch (err) {
+      const message = err.message.replace(/['"]+/g, '')
+      const response = Response.badResponse(message, 400)
+
+      return res.status(400).send(response)
     }
+  }
 
-    static async getQuestionByLevel(req, res) {
-        try {
-            const questions = await questionServices.getQuestionByLevel(req.params.level)
-            
-            if (!questions) throw new Error('Error while getting questions')
+  static async getQuestionByLevel (req, res) {
+    const token = req.headers.authorization
+    try {
+      // Check if token exist
+      if (!token) throw new Error('Token is required')
 
-            const response = Response.successResponse(200, 'Questions retrieved successfully', questions)
-            return res.status(200).send(response)
-        } catch (err) {
-            const message = err.message.replace(/['"]+/g, '')
-            const response = Response.badResponse(message, 400)
+      // validate token
+      const authorization = Tokenization.verifyToken(token)
 
-            return res.status(400).send(response)
-        }
+      if (!authorization) throw new Error('Invalid token')
+
+      const questions = await questionServices.getQuestionByLevel(req.params.level)
+
+      if (!questions) throw new Error('Error while getting questions')
+
+      const response = Response.successResponse(200, 'Questions retrieved successfully', questions)
+
+      return res.status(200).send(response)
+    } catch (err) {
+      const message = err.message.replace(/['"]+/g, '')
+      const response = Response.badResponse(message, 400)
+      return res.status(400).send(response)
     }
+  }
 }
 
 module.exports = questionController
--- src/services/questionServices.js
+++ src/services/questionServices.js
@@ -1,29 +1,35 @@
 const Database = require('../database/database')
 
 class questionServices {
-    static async getAllQuestions(req, res) {
-        await Database.createConnection()
-        const query = 'SELECT * FROM questions'
-        const questions = await Database.query(query)
-        await Database.close()
-        return questions[0]
-    }
+  static async getAllQuestions () {
+    await Database.createConnection()
+    const query = 'SELECT * FROM questions'
+    const questions = await Database.query(query)
+    await Database.close()
+    return questions[0]
+  }
 
-    static async getQuestionById(req, res) {
-        await Database.createConnection()
-        const query = `SELECT * FROM questions WHERE id = "${req}"`
-        const question = await Database.query(query)
-        await Database.close()
-        return question[0]
+  static async getQuestionById (id) {
+    await Database.createConnection()
+    const query = {
+      sql: 'SELECT * FROM questions WHERE id = ?',
+      values: [id]
     }
+    const question = await Database.query(query)
+    await Database.close()
+    return question[0]
+  }
 
-    static async getQuestionByLevel(req, res) {
-        await Database.createConnection()
-        const query = `SELECT * FROM questions WHERE question_level = "${req}"`
-        const question = await Database.query(query)
-        await Database.close()
-        return question[0]
+  static async getQuestionByLevel (level) {
+    await Database.createConnection()
+    const query = {
+      sql: 'SELECT * FROM questions WHERE idlevel = ?',
+      values: [level]
     }
+    const question = await Database.query(query)
+    await Database.close()
+    return question[0]
+  }
 }
 
 module.exports = questionServices
