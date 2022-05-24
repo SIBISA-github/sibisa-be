0a3030efdeb2ff239495c449bf1bdeb307586eff
Faster112 <muhammadtauchid@gmail.com>
2022-05-24 17:54:03 +0700

    feat: add lessonController

---
+++ src/controllers/lessonController.js
@@ -0,0 +1,96 @@
+const lessonServices = require('../services/lessonServices')
+const Tokenization = require('../utils/jwtToken/tokenization')
+const Response = require('../utils/response/response')
+const PayloadValidator = require('../utils/validator')
+
+class lessonController {
+  static async getAllLessons (req, res) {
+    const token = req.headers.authorization
+
+    try {
+      // Check if token exist
+      if (!token) throw new Error('Token is required')
+
+      // validate token
+      const authorization = Tokenization.verifyToken(token)
+
+      if (!authorization) throw new Error('Invalid token')
+
+      const lessons = await lessonServices.getAllLessons()
+
+      if (!lessons) throw new Error('Error while getting lessons')
+
+      const response = Response.successResponse(200, 'Lessons retrieved successfully', lessons)
+
+      return res.status(200).send(response)
+    } catch (err) {
+      const message = err.message.replace(/['"]+/g, '')
+
+      const response = Response.badResponse(message, 400)
+
+      return res.status(400).send(response)
+    }
+  }
+
+  static async getLessonByID (req, res) {
+    const token = req.headers.authorization
+
+    try {
+      // Check if token exist
+      if (!token) throw new Error('Token is required')
+
+      // validate token
+      const authorization = Tokenization.verifyToken(token)
+
+      if (!authorization) throw new Error('Invalid token')
+
+      // validate parameter
+      PayloadValidator.validateLessonID(req.params.id)
+
+      const lesson = await lessonServices.getLessonByID(req.params.id)
+
+      if (!lesson) throw new Error('Error while getting lessons')
+
+      const response = Response.successResponse(200, 'Lessons retrieved successfully', lesson)
+
+      return res.status(200).send(response)
+    } catch (err) {
+      const message = err.message.replace(/['"]+/g, '')
+      const response = Response.badResponse(message, 400)
+
+      return res.status(400).send(response)
+    }
+  }
+
+  static async getLessonByLevel (req, res) {
+    const token = req.headers.authorization
+
+    try {
+      // Check if token exist
+      if (!token) throw new Error('Token is required')
+
+      // validate token
+      const authorization = Tokenization.verifyToken(token)
+
+      if (!authorization) throw new Error('Invalid token')
+
+      // validate parameter
+      PayloadValidator.validateLevelID(req.params.level)
+
+      const lesson = await lessonServices.getLessonByLevel(req.params.level)
+
+      if (!lesson) throw new Error('Error while getting lessons')
+
+      const response = Response.successResponse(200, 'Lessons retrieved successfully', lesson)
+
+      return res.status(200).send(response)
+    } catch (err) {
+      const message = err.message.replace(/['"]+/g, '')
+      const response = Response.badResponse(message, 400)
+
+      return res.status(400).send(response)
+    }
+  }
+}
+
+module.exports = lessonController
