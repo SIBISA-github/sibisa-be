c15f567ce03e1fdce037724ce28f6e1034fd88a5
Faster112 <muhammadtauchid@gmail.com>
2022-05-24 19:32:32 +0700

    fix: bug token

--- src/utils/jwtToken/tokenization.js
+++ src/utils/jwtToken/tokenization.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 class Tokenization {
   static makeToken (payload) {
     // Create jwt token
-    return jwt.sign(payload, process.env.JWT_SECRET)
+    return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '7d' })
   }
 
   static verifyToken (token) {
