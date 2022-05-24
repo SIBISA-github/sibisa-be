ee89401d1947cd421c5acbbfd8ffc2235a20d4ae
Faster112 <muhammadtauchid@gmail.com>
2022-05-24 12:53:24 +0700

    fix: change to eslint format and change parameter

--- src/services/questionServices.js
+++ src/services/questionServices.js
@@ -1,29 +1,29 @@
 const Database = require('../database/database')
 
 class questionServices {
-    static async getAllQuestions(req, res) {
-        await Database.createConnection()
-        const query = 'SELECT * FROM questions'
-        const questions = await Database.query(query)
-        await Database.close()
-        return questions[0]
-    }
+  static async getAllQuestions () {
+    await Database.createConnection()
+    const query = 'SELECT * FROM questions'
+    const questions = await Database.query(query)
+    await Database.close()
+    return questions[0]
+  }
 
-    static async getQuestionById(req, res) {
-        await Database.createConnection()
-        const query = `SELECT * FROM questions WHERE id = "${req}"`
-        const question = await Database.query(query)
-        await Database.close()
-        return question[0]
-    }
+  static async getQuestionById (id) {
+    await Database.createConnection()
+    const query = `SELECT * FROM questions WHERE id = "${id}"`
+    const question = await Database.query(query)
+    await Database.close()
+    return question[0]
+  }
 
-    static async getQuestionByLevel(req, res) {
-        await Database.createConnection()
-        const query = `SELECT * FROM questions WHERE question_level = "${req}"`
-        const question = await Database.query(query)
-        await Database.close()
-        return question[0]
-    }
+  static async getQuestionByLevel (level) {
+    await Database.createConnection()
+    const query = `SELECT * FROM questions WHERE question_level = "${level}"`
+    const question = await Database.query(query)
+    await Database.close()
+    return question[0]
+  }
 }
 
 module.exports = questionServices
